a8ac4370e8ab271218450719ccb31f3f02f59c7f
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2021-03-18 00:34:41 +0200

    Adding code editor

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
-
+import CodeEditor from "./components/code-editor";
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
@@ -64,6 +64,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
